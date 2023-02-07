db8311e5ac0a3e9c3ca0a194f63aa4be34c9e590
Almaz <almazius11@gmail.com>
2023-02-07 19:06:03 +0300

    use api and  created dynamic corrency header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React, { useEffect, useState } from 'react'
+import { Block } from './Block'
+import './index.scss'
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('RUB')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [rates, setRates] = useState({})
+
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: { apikey: 'jKzbk07S4YZ1Rm3i5YQ1evWGmim7ggqV' },
+    }
+
+    fetch(
+      'https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=USD&amount=10',
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => setRates(result))
+      .catch((error) => console.warn(error))
+  }, [rates])
+
+  console.log(rates)
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={0}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+      />
+      <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// jKzbk07S4YZ1Rm3i5YQ1evWGmim7ggqV
