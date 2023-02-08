21f141bbc914aa92b282d0039bfa426b9de48296
Almaz <almazius11@gmail.com>
2023-02-08 19:10:18 +0300

    created func onChangeFromPrice and onChangeToPrice

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Block } from './Block'
 import './index.scss'
 
 function App() {
-  const [fromCurrency, setFromCurrency] = useState('RUB')
-  const [toCurrency, setToCurrency] = useState('USD')
   const [rates, setRates] = useState({})
 
+  const [fromCurrency, setFromCurrency] = useState('RUB')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [fromPrice, setFromPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(0)
+
   useEffect(() => {
-    var requestOptions = {
+    const requestOptions = {
       method: 'GET',
       redirect: 'follow',
       headers: { apikey: 'jKzbk07S4YZ1Rm3i5YQ1evWGmim7ggqV' },
@@ -21,18 +24,35 @@ function App() {
       .then((response) => response.text())
       .then((result) => setRates(result))
       .catch((error) => console.warn(error))
-  }, [rates])
+  }, [])
 
-  console.log(rates)
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency]
+    const result = price * rates[toCurrency]
+    setToPrice(result)
+    setFromPrice(value)
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (rates[fromCurrency] / rates[toCurrency]) * value
+    setFromPrice(result)
+    setToPrice(value)
+  }
 
   return (
     <div className="App">
       <Block
-        value={0}
+        value={fromPrice}
         currency={fromCurrency}
         onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
       />
-      <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   )
 }
--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+const defaultCurrencies = ['RUB', 'USD', 'EUR', 'TRY']
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
   <div className="block">
@@ -9,7 +9,8 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
         <li
           onClick={() => onChangeCurrency(cur)}
           className={currency === cur ? 'active' : ''}
-          key={cur}>
+          key={cur}
+        >
           {cur}
         </li>
       ))}
@@ -27,4 +28,4 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
       placeholder={0}
     />
   </div>
-);
+)
